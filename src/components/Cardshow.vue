2c899f76dd7668a8ce71bd081219ef7737ee923e
EdoardoRagazzi <edo.ragazzi@gmail.com>
2021-07-13 14:56:38 +0200

    Movies / TV-Shows

---
+++ src/components/Cardshow.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card">
+    <ul>
+      <li>Titolo: {{ info.title }}</li>
+      <li>Titolo.Originale: {{ info.original_title }}</li>
+      <li>Lingua: {{ info.original_language }}</li>
+      <li>Voto: {{ info.vote_average }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  props: ["info"],
+};
+</script>
+
+
+<style scoped lang="scss">
+ul {
+  list-style: none;
+}
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,16 +1,25 @@
 <template>
   <div class="main">
     <h2>Hai Cercato: {{ campoRicerca }}</h2>
-    <Card v-for="film in films" :key="film.id" :info="film" />
+    <div class="movies">
+      <h2>Movies</h2>
+      <Card v-for="film in films" :key="film.id" :info="film" />
+    </div>
+    <div class="tv-show">
+      <h2>Tv-show</h2>
+      <Cardshow v-for="film in films" :key="film.id" :info="film" />
+    </div>
   </div>
 </template>
 
 <script>
 import Card from "@/components/Card.vue";
+import Cardshow from "@/components/Cardshow.vue";
 export default {
   name: "Main",
   components: {
     Card,
+    Cardshow,
   },
   props: {
     films: Array,
@@ -21,4 +30,10 @@ export default {
 
 
 <style scoped lang="scss">
+.movies {
+  float: left;
+}
+.tv-show {
+  float: right;
+}
 </style>
